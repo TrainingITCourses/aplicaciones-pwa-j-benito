47641217c5ce4c31c96ba4db3854c8620290efb4
Javier <jbenito.dz@gmail.com>
2018-10-15 21:59:41 +0200

    Added CDN to ngsw

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { SwUpdate, UpdateAvailableEvent } from '@angular/service-worker';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -7,4 +8,20 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public developerVersion = '4';
+  constructor(swUpdate: SwUpdate) {
+    if (swUpdate.isEnabled) {
+      swUpdate.available.subscribe(
+        (event: UpdateAvailableEvent) => {
+          const msg = 
+            'current: ' +
+            event.current.hash +
+            '. Load new: ' +
+            event.available.hash +
+            ' ?';
+          if (confirm(msg)) { window.location.reload(); }
+        }
+      );
+    }
+  }
 }
